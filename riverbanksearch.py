ef6fbc848f364753a79aee1539a71cc38355e573
kurian <kurianos@gmail.com>
2012-07-05 00:24:05 -0700

    first version of pyqt search

---
+++ riverbanksearch.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+__authors__ = ["kurian.os"]
+__version__ = '$Revision: 71504 $'.split()[1]
+__revision__ = __version__ # For pylint
+__date__ = '$Date:  July 5, 2012 12:00:00 PM$'.split()[1]
+__copyright__ = '2011'
+__license__ = "Copyright 2011 Kurian"
+__contact__ = "kurianos@gmail.com"
+__status__ = "Development"
+
+import sublime
+import sublime_plugin
+
+import subprocess
+import webbrowser
+
+def SearchFor(text):
+    url = 'http://www.riverbankcomputing.co.uk/static/Docs/PyQt4/html/%s.html'%text.lower()
+    webbrowser.open_new_tab(url)
+
+class RiverbankSearchCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for selection in self.view.sel():
+            if selection.empty():
+                text = self.view.word(selection)
+            text = self.view.substr(selection)
+            SearchFor(text)
